afa41c2a683f96d3eae027cbd7cf2d5fde291788
Isabelle Eloise Rugno Ferreira <Isabelle.eloise4369@gmail.com>
2024-09-06 16:19:13 -0300

    Front-Página_Inicial 06.09

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,51 +1,17 @@
-// Componente Menu
-
-document.addEventListener('DOMContentLoaded', async function () {
-    try {
-        const codMenu = await fetch('/static/menu-home/menu-home.html');
-        // const codMenu = await fetch('./templates/menu-home.html');
-        if (!codMenu.ok) {
-            throw new Error('Arquivo não encontrado no servidor.');
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
         }
-        const menu = await codMenu.text();
-        document.querySelector('.container-menu').innerHTML = menu;
-        initMenuMobile();
-    } catch (error) {
-        console.error('Erro ao carregar o menu: ', error);
+      }
     }
-});
-
-
-// Função para inicializar os eventos do menu mobile
-const initMenuMobile = () => {
-
-    const  btnMobile = document.querySelector('#btn-mobile');
-    if (!btnMobile) {
-        console.error('#btn-mobile não encontrado');
-        return;
-    }
-    const toggleMenu = (event) => {
-        // BL 01
-        if (event.type === 'touchstart') event.preventDefault();
-    
-        // Adicionar a classe acttive, caso não tenha.
-        // Remover a classe acttive, caso tenha
-        // BL 02
-        const nav = document.getElementById('nav'); 
-        nav.classList.toggle('active');
-        const active = nav.classList.contains('active');
-
-        // BL 03
-        event.currentTarget.setAttribute('aria-expanded', active);
-        if(active){
-            event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-        }else{
-            event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    
-        }
-    }
-    
-    btnMobile.addEventListener('click', toggleMenu);
-    btnMobile.addEventListener('touchstart', toggleMenu);
-}
-
+  }
